0fa84ef05b6582fc3c18800a794ffef49fdb6b69
Ulukbek <l9l9ndos@gmail.com>
2024-04-02 04:00:38 +0600

    Set up admin

--- apps/collects/admin.py
+++ apps/collects/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Collect, Payment
 
-# Register your models here.
+
+@admin.register(Collect)
+class CollectAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'title',
+        'occasion',
+        'target_amount',
+        'current_amount',
+        'contributors_count',
+        'end_date'
+    )
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'collect',
+        'contributor',
+        'amount',
+        'timestamp'
+    )
