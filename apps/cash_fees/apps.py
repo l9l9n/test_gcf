f054cc938d05eecf9d8bc3c13c0424d637eab34c
Ulukbek <l9l9ndos@gmail.com>
2024-04-01 23:21:51 +0600

    Create app cash_fees

---
+++ apps/cash_fees/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CashFeesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.cash_fees'
--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for config project.
+ASGI group_cash_fees for group_cash_fees project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_cash_fees.settings')
 
 application = get_asgi_application()
